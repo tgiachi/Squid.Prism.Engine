3af2b79349016d84ff731a14692480d829222103
squid <squid@stormwind.it>
2025-01-30 11:12:01 +0100

    feat(NetworkMessageMapService.cs): add support for registering and retrieving message descriptions in NetworkMessageMapService feat(INetworkMessageMapService.cs): update RegisterMessage method to include optional message description parameter

--- src/Squid.Prism.Network/Impl/Encoders/NetworkMessageMapService.cs
+++ src/Squid.Prism.Network/Impl/Encoders/NetworkMessageMapService.cs
@@ -10,13 +10,14 @@ public class NetworkMessageMapService : INetworkMessageMapService
     private readonly ILogger _logger;
 
     private readonly Dictionary<int, Type> _messageMap = new();
+    private readonly Dictionary<int, string> _messageMapDescriptions = new();
 
     public NetworkMessageMapService(ILogger<NetworkMessageMapService> logger)
     {
         _logger = logger;
     }
 
-    public void RegisterMessage<TMessage>(int messageType) where TMessage : INetworkMessage
+    public void RegisterMessage<TMessage>(int messageType, string? description = null) where TMessage : INetworkMessage
     {
         _logger.LogDebug("Registering message type {MessageType} with {Message}", messageType, typeof(TMessage).Name);
 
@@ -25,6 +26,10 @@ public class NetworkMessageMapService : INetworkMessageMapService
             _logger.LogWarning("Message type {MessageType} is already registered", messageType);
             return;
         }
+
+        description ??= typeof(TMessage).Name;
+
+        _messageMapDescriptions.Add(messageType, description);
 
         _messageMap.Add(messageType, typeof(TMessage));
     }
@@ -38,5 +43,16 @@ public class NetworkMessageMapService : INetworkMessageMapService
         }
 
         return messageTypeValue;
+    }
+
+    public string GetMessageDescription(int messageType)
+    {
+        if (!_messageMapDescriptions.TryGetValue(messageType, out var messageDescription))
+        {
+            _logger.LogError("Message type {MessageType} is not registered", messageType);
+            throw new KeyNotFoundException($"Message type {messageType} is not registered");
+        }
+
+        return messageDescription;
     }
 }
--- src/Squid.Prism.Network/Interfaces/Services/INetworkMessageMapService.cs
+++ src/Squid.Prism.Network/Interfaces/Services/INetworkMessageMapService.cs
@@ -5,7 +5,9 @@ namespace Squid.Prism.Network.Interfaces.Services;
 
 public interface INetworkMessageMapService
 {
-    void RegisterMessage<TMessage>(int messageType) where TMessage : INetworkMessage;
+    void RegisterMessage<TMessage>(int messageType, string? description = null) where TMessage : INetworkMessage;
 
     Type GetMessageType(int messageType);
+
+    string GetMessageDescription(int messageType);
 }
