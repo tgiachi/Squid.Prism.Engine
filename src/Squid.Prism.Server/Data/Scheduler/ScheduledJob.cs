e22f2cb60336582c31cd8a5c4c38668a02730044
squid <squid@stormwind.it>
2025-02-03 11:18:39 +0100

    feat: removed server app console for make library

--- src/Squid.Prism.Server/Data/Scheduler/ScheduledJob.cs
+++
@@ -1,9 +0,0 @@
-namespace Squid.Prism.Server.Data.Scheduler;
-
-public class ScheduledJob
-{
-    public string Name { get; set; }
-    public TimeSpan Interval { get; set; }
-    public Func<Task> Task { get; set; }
-    public IDisposable Subscription { get; set; }
-}
