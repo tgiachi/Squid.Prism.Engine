c14b46316acc7543114660f19676a5f12d2b3635
squid <squid@stormwind.it>
2025-02-05 15:52:17 +0100

    refactor(INetworkClientService.cs): remove unnecessary whitespace to improve code readability

--- src/Squid.Prism.Network.Client/Interfaces/INetworkClientService.cs
+++ src/Squid.Prism.Network.Client/Interfaces/INetworkClientService.cs
@@ -8,6 +8,5 @@ public interface INetworkClientService : INotifyPropertyChanged
 {
     public Vector3 Position { get; set; }
     public Vector3 Rotation { get; set; }
-    
     public Subject<Vector3> PositionSubject { get; }
 }
