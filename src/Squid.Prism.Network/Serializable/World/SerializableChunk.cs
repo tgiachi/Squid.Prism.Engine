4308fd9efa6f87e0ff0637f576d0cd1c23f8beeb
squid <squid@stormwind.it>
2025-02-06 14:32:43 +0100

    feat(SerializableChunk.cs): add SerializableVector3 Position property and List<byte> Blocks property to improve serialization of chunk data feat(SerializableChunk.cs): add constructor to initialize Position and Blocks properties based on ChunkEntity data

--- src/Squid.Prism.Network/Serializable/World/SerializableChunk.cs
+++ src/Squid.Prism.Network/Serializable/World/SerializableChunk.cs
@@ -1,10 +1,31 @@
+using System.Numerics;
 using ProtoBuf;
+using Squid.Prism.Engine.Core.Data.World;
+using Squid.Prism.Engine.Core.World;
+using Squid.Prism.Network.Serializable.Math;
 
 namespace Squid.Prism.Network.Serializable.World;
 
 [ProtoContract]
 public class SerializableChunk
 {
+    [ProtoMember(1)] public SerializableVector3 Position { get; set; }
+
+    [ProtoMember(2)] public List<byte> Blocks { get; set; } = new List<byte>();
+
+    public SerializableChunk()
+    {
+    }
+
+    public SerializableChunk(Vector3 position, ChunkEntity chunkEntity)
+    {
+        Position = position;
+
+        foreach (var block in chunkEntity.Blocks)
+        {
+            Blocks.Add(block.Id);
+        }
+    }
 
 
 }
